994b74174cbe1fca7a332b3137e8afbcdf2ae356
mrwesleycrusher <wesleywyatttaylor@gmail.com>
2019-03-31 18:32:13 -0600

    added instructions for File Server usage and a network driver implementation that hasn't been tested

---
+++ networkingBackend/File Server with loopback/README.md
@@ -0,0 +1,11 @@
+Usage:
+
+1. Open two terminal tabs
+2. Run Server.py in one terminal
+3. Run Client.py in the other
+4. In the client enter the absolute path to a file
+5. See the copy that was made at that path, with the original name appended as .new
+
+Troubleshooting:
+
+If you get an error opening the socket, ensure port 5000 is open on your machine and that it isn't currently being used by a different instance of the application
---
+++ networkingBackend/Network Driver Implementation/NetworkDriver.py
@@ -0,0 +1,65 @@
+#this class runs on the host computer to connect it to the
+#raspi zero to abstract the biolerplate of connecting, sending
+#commands, and recieving images from the pi camera
+import socket
+import subprocess
+import re, threading
+
+
+class NetworkDriver:
+    def __init__(self, type):
+        if type == 'laptop':
+            self.lock = threading.Lock()
+            self.port = 5000
+            #find raspberry pi ip address
+            p1 = subprocess.Popen(['cat', '/var/lib/misc/dnsmasq.leases'], stdout=subprocess.PIPE)
+            #assign host to the ip address contained in the output of the subprocess
+            self.host = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",p1.communicate()[0].decode())[0]
+            self.sock = socket.socket()
+            self.sock.connect((self.host, self.port))
+        if type == 'pi':
+            self.lock = threading.Lock()
+            self.host = socket.gethostname() 
+            self.port = 5000
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.bind(('',self.port))
+            #look for backlog
+            self.sock.listen(5)
+            #accept incoming connection, since we don't support multiple 
+            #clients at once, just assign sock to the connection
+            self.sock, self.clientIP = self.sock.accept()
+            self.mostRecentCommand = None
+            self.killthread = False
+
+
+    #takes an unencoded string and sends it to the server
+    def send(self, information):
+        with self.lock:
+            #we need to tell the other end how much we are sending
+            self.sock.sendall(str(len(information.encode())).encode())
+            #check for 'ack'
+            ack = None 
+            while ack != 'ack':
+                ack = socket.recieve(1024).decode()
+
+            #now we need to actually send the data over the socket
+            self.sock.sendall(information.encode())
+            ack = None
+            #now of course we wait for ack again
+            while ack != 'ack':
+                ack = socket.recieve(1024).decode()
+        
+
+    #recieves an unencoded string from the server
+    def recieve(self):
+        with self.lock:
+            transferSize = int(self.sock.recv(1024).decode())
+            self.socket.sendall('ack'.encode())
+            transfer = int(self.sock.recv(transferSize).decode())
+            self.socket.sendall('ack'.encode())
+            return transfer
+            
+    
+    def __del__(self):
+        with self.lock:
+            self.sock.close()
