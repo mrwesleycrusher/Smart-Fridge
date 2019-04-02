5ac84e16192df5bab20e371e2664df5816044499
tcengle <tengle314@gmail.com>
2019-04-02 00:11:01 -0600

    added more performance tests

--- LCD/test/performance.c
+++ LCD/test/performance.c
@@ -12,11 +12,48 @@ int main(){
     t = clock() - t;
     time_taken = ((double)t)/CLOCKS_PER_SEC;
     printf("\nInit finished in %f seconds",time_taken);
-    t = clock();
-    fridge_display(disp,"ripipipipipipipipipipipipipipipipip");
-    t = clock() - t;
-    time_taken = ((double)t)/CLOCKS_PER_SEC;
-    printf("\nDisplay finished in %f seconds",time_taken);
+    char testc = ' ';
+    double average = 0;
+    char *c;
+    int i = 0;
+    printf("\nSingle Character Test\n-----------------------------s");
+    for(i = 0; i!=0x7f; i++){
+        t = clock();
+        c = &testc;
+        fridge_display(disp,c);
+        t = clock() - t;
+        time_taken = ((double)t)/CLOCKS_PER_SEC;
+        average += time_taken;
+        //printf("\n%f",time_taken);
+        testc++;
+    }
+    average /= i;
+    printf("\nSingle chars take %f seconds\n",average);
+
+    printf("\n32 Character Test\n-----------------------------");
+    for(i = 0; i < 100; i++){
+        t = clock();
+        fridge_display(disp,"12345678901234567890123456789012");
+        t = clock() - t;
+        time_taken = ((double)t)/CLOCKS_PER_SEC;
+        average += time_taken;
+        //printf("\n%f",time_taken);
+    }
+    average /= i;
+    printf("\n32 Chars take %f seconds\n",average);
+    
+    printf("\n32 Character With Clear Test\n-----------------------------");
+    for(i = 0; i < 100; i++){
+        t = clock();
+        fridge_display(disp,"12345678901234567890123456789012");
+        fridge_clear(disp);
+        t = clock() - t;
+        time_taken = ((double)t)/CLOCKS_PER_SEC;
+        average += time_taken;
+        //printf("\n%f",time_taken);
+    }
+    average /= i;
+    printf("\n32 Chars take %f seconds",average);
     return 0;
 
 }
