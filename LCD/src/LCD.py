cbb98d9d4fb4adb1544038d5e7a1c347715c5ae1
tcengle <tengle314@gmail.com>
2019-04-29 22:04:03 -0600

    Added documentation

--- LCD/src/LCD.py
+++ LCD/src/LCD.py
@@ -1,15 +1,25 @@
 from RPi import GPIO
 from RPLCD.gpio import CharLCD
 
+
+#######################################
+#               LCD
+#Abstracts RPLCD lcd methods
+#######################################
 class LCD():
     
     def __init__(self):
         self.lcd = CharLCD(pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode = GPIO.BOARD, cols=16, rows=2, charmap='A02', auto_linebreaks=True)
         self.lcd.write_string("God is Dead")
-
+    
+#######################################
+#               display
+#Displays passed string in LCD
+#######################################
     def display(self, str):
         self.lcd.clear()
         self.lcd.write_string(str)
 
+#quick test code
 # q = LCD()
 # q.display("uwu")
--- LCD/src/scanInput.py
+++ LCD/src/scanInput.py
@@ -1,10 +1,25 @@
+from RPi import GPIO
+import time
 
-
+#######################################
+#               scannerInput
+#Opens a text field in the terminal
+#and reads in a code
+#REQUIRES THE TERMINAL BE FOCUSED WHEN READING
+#######################################
 class scannerInput():
 
     def __init__(self):
-        print "Scanner done"
+        print "Scanner up"
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    ###################################
+    #           scanForCode
+    #Focuses the text field and allows
+    #allows scanner to input code
+    #Returns: Code
+    ###################################
     def scanForCode(self):
         try:
             code = raw_input("Enter code: ")
@@ -12,7 +27,24 @@ class scannerInput():
 
         except KeyboardInterrupt:
             print "Failed Read"
+
+    ###################################
+    #           getAddState
+    #Returns if button is in add state
+    #True: button is in add state
+    #False: button is in subtract state
+    ###################################
+    def getAddState(self):
+        input_state = GPIO.input(18)
+        return input_state
         
+
+#Quick test code
 # q = scannerInput()
 # bc = q.scanForCode()
 # print bc
+# while True:
+#     state = q.getAddState()
+#     print state
+#     time.sleep(0.2)
+
