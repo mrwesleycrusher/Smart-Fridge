1f7593b264e8fa40b83199ade103d7a4090cbf35
Wes <wesleywyatttaylor@gmail.com>
2019-05-02 10:50:23 -0600

    I am really upset about how python does globals

--- Client PC/main.py
+++ Client PC/main.py
@@ -8,27 +8,29 @@ import signal
 grocery_list
 
 def timeout_handler(signum, frame):
-    send_to_email("Wow@wow.com", get_the_message(global grocery_list))
+    global grocery_list
+    send_to_email("Wow@wow.com", get_the_message(grocery_list))
     save(global grocery_list)
     exit()
 
 def main():
     network = NetworkDriver('laptop')
     continue = True
-    global grocery_list = load()
+    global grocery_list 
+    grocery_list= load()
     
     while(continue):
         signal.alarm(600)
         next_barcode = network.recieve()
         switch_state = network.recieve()
 
-        if global grocery_list[next_barcode] is not None:
+        if grocery_list[next_barcode] is not None:
                 if switch_state == "add":
-                    global grocery_list[next_barcode] = global grocery_list[next_barcode] + Foodstuff(next_barcode)
+                    grocery_list[next_barcode] = grocery_list[next_barcode] + Foodstuff(next_barcode)
                 else:
-                    global grocery_list[next_barcode] = Foodstuff(next_barcode)
+                    grocery_list[next_barcode] = Foodstuff(next_barcode)
 
-        network.send(global grocery_list[next_barcode].get_name())
+        network.send(grocery_list[next_barcode].get_name())
 
 if __name__=="__main__":
     main()
