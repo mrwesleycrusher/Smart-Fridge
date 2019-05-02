2784365a480695581836a76b10381bbd2249fc03
Wes <wesleywyatttaylor@gmail.com>
2019-05-02 10:02:50 -0600

    I am not even into globals

--- Fridge/main.py
+++ Fridge/main.py
@@ -10,7 +10,7 @@ buttonState=1
 def pollButton():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    buttonState = GPIO.input(18)
+    global buttonState = GPIO.input(18)
 
 def timeout_handler(signum, frame):
     exit()
@@ -22,11 +22,13 @@ def main():
     me = True
     buttonpoller = threading.Thread(target=pollButton)
     buttonpoller.start()
+    global buttonState
     
     while(me):
         signal.alacrm(600)
         network.send(scanner.scanForCode())
-        if global buttonState == 1:
+        
+        if buttonState == 1:
             network.send("add")
         else:
             network.send("something else")
