79ba75444a11a62c284b52bbeeed7fa15bbf152a
tcengle <tengle314@gmail.com>
2019-05-03 12:57:03 -0600

    used Ty's email

--- Client PC/main.py
+++ Client PC/main.py
@@ -1,6 +1,6 @@
 from barcodeProcessing.Food import Foodstuff
 from sendEmail.SendMessage import send_to_email, get_the_message
-from storage.StoreToDisk import save, load
+from storage.StoreToDisk import save, load, pickleSave
 from networkingBackend.NetworkDriver import NetworkDriver
 import json
 import signal
@@ -9,20 +9,22 @@ grocery_list = {}
 
 def timeout_handler(signum, frame):
     global grocery_list
-    send_to_email("Wow@wow.com", get_the_message(grocery_list))
-    save(grocery_list)
+    print("Sending Email")
+    send_to_email("monzohno@gmail.com", get_the_message(grocery_list))
+    pickleSave(grocery_list)
     exit()
 
 def main():
     network = NetworkDriver('laptop')
+    signal.signal(signal.SIGALRM, timeout_handler)
     me = True
     global grocery_list 
     grocery_list= load()
     
     while(me):
-        signal.alarm(600)
-        next_barcode = network.recieve()
-        switch_state = network.recieve()
+        signal.alarm(10)
+        next_barcode = network.receive()
+        switch_state = network.receive()
 
         grocery_list.setdefault(next_barcode, Foodstuff(next_barcode, 0.0)) # should add the key if it is not found
 
