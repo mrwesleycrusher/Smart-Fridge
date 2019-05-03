9cd47d40b1936f59654d71ef65f0b7992d8d0893
tcengle <tengle314@gmail.com>
2019-05-03 15:42:25 -0600

    updated save to serialize object correctly

--- Client PC/storage/StoreToDisk.py
+++ Client PC/storage/StoreToDisk.py
@@ -3,12 +3,12 @@ import pickle
 
 def save(json_data):
     with open('grocery_list_backup.json', 'w') as outfile:
-        json.dump(json_data, outfile)
+        json.dump([value.__dict__ for key,value in json_data.items()], outfile)
 
 def load():
     with open('grocery_list_backup.json', 'r') as infile:
         return json.loads(infile.read())
 
-def pickleSave(data):
-        with open('grocery_list_backup.json', 'w') as outfile:
-                pickle.dump(data, outfile)
+# def pickleSave(data):
+#         with open('grocery_list_backup.json', 'wb') as outfile:
+#                 pickle.dump(data, outfile)
