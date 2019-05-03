d16558f37766a19e3b5e4b8a5ddba796687b2445
tcengle <tengle314@gmail.com>
2019-05-03 12:57:27 -0600

    tried pickle

--- Client PC/storage/StoreToDisk.py
+++ Client PC/storage/StoreToDisk.py
@@ -1,4 +1,6 @@
 import json
+import pickle
+
 def save(json_data):
     with open('grocery_list_backup.json', 'w') as outfile:
         json.dump(json_data, outfile)
@@ -6,3 +8,7 @@ def save(json_data):
 def load():
     with open('grocery_list_backup.json', 'r') as infile:
         return json.loads(infile.read())
+
+def pickleSave(data):
+        with open('grocery_list_backup.json', 'w') as outfile:
+                pickle.dump(data, outfile)
