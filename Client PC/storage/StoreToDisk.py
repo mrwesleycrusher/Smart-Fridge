e46cc415128c3346eae61d0bda0631490adf790d
tcengle <tengle314@gmail.com>
2019-05-04 11:48:37 -0600

    heck

--- Client PC/storage/StoreToDisk.py
+++ Client PC/storage/StoreToDisk.py
@@ -1,13 +1,31 @@
 import json
 import pickle
+from barcodeProcessing.Food import Foodstuff
 
 def save(json_data):
-    with open('grocery_list_backup.json', 'w') as outfile:
-        json.dump([value.__dict__ for key,value in json_data.items()], outfile)
+    if json_data:
+        with open('grocery_list_backup.json', 'w') as outfile:
+                #print("Hecky you: " + [(value.__dict__ for key,value in json_data.items())])
+                json.dump(([value.__dict__ for key,value in json_data.items()]), outfile)
+                # json.dump(json_data, outfile)
 
 def load():
-    with open('grocery_list_backup.json', 'r') as infile:
-        return json.loads(infile.read())
+        with open('grocery_list_backup.json', 'r') as infile:
+                return_list = {}
+                s = json.loads(infile.read())
+                
+                if s:
+                        # print(s)            
+                        for x in s:
+                                # print(x)
+                                temp = Foodstuff(x['code'],int(x['quantity']))
+                                return_list.update({x['code']:temp}) 
+                                # print("LIST: ")
+                                # print(return_list)
+                        print("List Loaded")        
+                        return return_list
+                else:
+                        return s
 
 # def pickleSave(data):
 #         with open('grocery_list_backup.json', 'wb') as outfile:
