55fbf9d55d362c184f48d13136f5316190abb097
tcengle <tengle314@gmail.com>
2019-05-06 16:47:04 -0600

    added code for removing zero items

--- Client PC/main.py
+++ Client PC/main.py
@@ -26,17 +26,21 @@ def receive_data(network):
     switch_state = network.receive()
 
     food_temp = Foodstuff(next_barcode, 0.0)
-    if(food_temp.get_name != "NoSuchCode"):
+    if(food_temp.get_name() != "NoSuchCode"):
         grocery_list.setdefault(next_barcode, food_temp)  # should add the key if it is not found
 
         if switch_state == "add":
             grocery_list[next_barcode] = grocery_list[next_barcode] + Foodstuff(next_barcode)
         else:
             grocery_list[next_barcode] = grocery_list[next_barcode] - Foodstuff(next_barcode)
-
-        network.send(grocery_list[next_barcode].get_name())
+            if(food_temp.get_num() == 0.0):
+                grocery_list.pop(next_barcode)
+        if(next_barcode in grocery_list):
+            network.send(grocery_list[next_barcode].get_name())
+        else:
+            network.send("Removed from list")
     else:
-        network.send("Scan Failed")
+        network.send("Scan Failed: No Such Code")
 
 def main():
     network = NetworkDriver('laptop')
